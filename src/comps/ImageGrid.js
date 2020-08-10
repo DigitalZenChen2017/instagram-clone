ce806028a5b638f65e366afda75748d1135c196c
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2020-08-10 16:03:17 -0400

    Added Framer Motion Animations

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -22,6 +22,7 @@ const ImageGrid = ({ setSelectedImg }) => {
               alt="uploadedpicture"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
             />
           </motion.div>
         ))}
